92971583d7ef9ddfbead90ccbf62de4ffb758303
shinbunbun <takamuneyuto@gmail.com>
2021-01-07 12:28:09 +0900

    add blockcerts-verifier

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@blockcerts/blockcerts-verifier/dist/main.js'
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
